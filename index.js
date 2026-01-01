585c628d89f3525c7d518295ce2c214001e93ae2
yennifer <shiksjayne95@gmail.com>
2026-01-01 13:08:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-//Write your code here
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.0,
+};
 
+function logAttendeeName(attendeeObj) {
+  console.log(attendeeObj.name);
+}
 
+function logTicketPrice(attendeeObj) {
+  console.log(attendeeObj.ticketPrice);
+}
 
-//Needed for the tests to work. Don't modify
+function updateTicketType(attendeeObj, newTicketType) {
+  attendeeObj.ticketType = newTicketType;
+}
+
+function updateTicketPrice(attendeeObj, newTicketPrice) {
+  attendeeObj.ticketPrice = newTicketPrice;
+}
+
+function removeEventProperty(attendeeObj) {
+  delete attendeeObj.event;
+}
+
+function addCheckedInProperty(attendeeObj) {
+  attendeeObj.checkedIn = true;
+}
+
+// Needed for the tests to work. Don't modify
 module.exports = {
   ...(typeof attendee !== 'undefined' && { attendee }),
   ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
